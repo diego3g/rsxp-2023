d025d576d880afe6a389ec6d120adc93e0209d0c
RennanD <rplaykl@gmail.com>
2023-04-04 18:59:42 -0300

    chore: configure intial routes structure

---
+++ apps/mobile/app/credential.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export default function Credential() {
+  return (
+    <View className="bg-zinc-900 flex-1 items-center justify-center">
+      <Text className="text-gray-100 font-bold text-2xl">Credencial</Text>
+    </View>
+  )
+}
