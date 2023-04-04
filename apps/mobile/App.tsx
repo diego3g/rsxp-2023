4a2a5fad7b6ff1b2eec72315c42f91c739489c50
melquisedeque-magalhaes <melqui.sodre15@gmail.com>
2023-04-04 20:37:56 -0300

    chore: config fonts and theme

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import { Loading } from './src/components/Loading'
 
 export default function App() {
+  const [isFontLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  })
+
+  if (!isFontLoaded) {
+    return <Loading />
+  }
+
   return (
-    <View className="flex-1 bg-zinc-900 items-center justify-center">
-      <Text className="text-white text-4xl font-bold">Hello World</Text>
-      <StatusBar style="light" />
+    <View className="flex-1 bg-gray-800 items-center justify-center">
+      <StatusBar style="light" backgroundColor="transparent" translucent />
+      <Text className="text-white text-4xl font-bold font-heading">
+        Hello World
+      </Text>
     </View>
   )
 }
--- apps/mobile/tailwind.config.js
+++ apps/mobile/tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          100: '#E1E1E6',
+          600: '#202024',
+          800: '#121214',
+          900: '#09090A',
+        },
+        purple: {
+          500: '#8257E5',
+        },
+        red: {
+          500: '#F75A68',
+        },
+      },
+      fontFamily: {
+        heading: 'Roboto_700Bold',
+        subtitle: 'Roboto_500Medium',
+        body: 'Roboto_400Regular',
+      },
+    },
   },
   plugins: [],
 }
