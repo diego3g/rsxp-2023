07d14e9cc9bbbc6048013497fdc02900ec787308
Lucas Carrias <lucascarrias@outlook.com>
2023-04-05 10:02:53 -0300

    chore: create mobile app path alias

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,7 +1,26 @@
 module.exports = function (api) {
   api.cache(true)
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ['nativewind/babel', require.resolve('expo-router/babel')],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "nativewind/babel",
+      require.resolve("expo-router/babel"),
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          alias: {
+            "@assets": "./src/assets",
+            "@components": "./src/components",
+            "@screens": "./src/screens",
+            "@utils": "./src/utils",
+            "@services": "./src/services",
+            "@hooks": "./src/hooks",
+            "@contexts": "./src/contexts",
+            "@routes": "./src/routes",
+          },
+        },
+      ],
+    ],
   }
 }
