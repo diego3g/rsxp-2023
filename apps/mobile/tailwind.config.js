dcd3574708cab64d3dd4f4298285bae01f39bb09
melquisedeque-magalhaes <melqui.sodre15@gmail.com>
2023-04-06 13:34:47 -0300

    chore:(adjust folders)

--- apps/mobile/tailwind.config.js
+++ apps/mobile/tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
     './App.{js,jsx,ts,tsx}',
     './src/**/*.{js,jsx,ts,tsx}',
     './app/**/*.{js,jsx,ts,tsx}',
-    './components/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     colors: {
