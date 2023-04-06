7780d94418f017eab77f227f30cd858bfbb4b1bd
melquisedeque-magalhaes <melqui.sodre15@gmail.com>
2023-04-06 13:42:39 -0300

    chore:(adjust lint and delete Loading component)

--- apps/mobile/app/components/DrawerItem.tsx
+++ apps/mobile/app/components/DrawerItem.tsx
@@ -1,39 +1,38 @@
-import { Text, TouchableOpacity, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import { SvgProps } from 'react-native-svg';
-import { Link } from 'expo-router';
+import React from 'react'
+import { Text, TouchableOpacity, View } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+import { SvgProps } from 'react-native-svg'
+import { Link } from 'expo-router'
 
-import colors from 'tailwindcss/colors';
+import colors from 'tailwindcss/colors'
 
 type Props = {
-  href: string;
-  title: string;
-  subtitle: string;
-  icon: React.FC<SvgProps>;
+  href: string
+  title: string
+  subtitle: string
+  icon: React.FC<SvgProps>
 }
 
-export default function DrawerItem({ title, subtitle, icon: Icon, href }: Props) {
-
+export default function DrawerItem({
+  title,
+  subtitle,
+  icon: Icon,
+  href,
+}: Props) {
   return (
     <Link href={href} asChild>
       <TouchableOpacity className="grow border-b py-4 border-b-gray-800 flex-row items-center">
         <Icon />
 
         <View className="ml-5 flex-1">
-          <Text className="text-gray-100 font-medium text-base">
-            {title}
-          </Text>
+          <Text className="text-gray-100 font-medium text-base">{title}</Text>
 
           <Text className="text-gray-300 text-sm" numberOfLines={1}>
             {subtitle}
           </Text>
         </View>
 
-        <Ionicons
-          name='chevron-forward'
-          size={28}
-          color={colors.gray[400]}
-        />
+        <Ionicons name="chevron-forward" size={28} color={colors.gray[400]} />
       </TouchableOpacity>
     </Link>
   )
--- apps/mobile/src/components/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import { View, ActivityIndicator } from 'react-native'
-
-export function Loading() {
-  return (
-    <View className="flex-1 bg-gray-900 items-center justify-center">
-      <ActivityIndicator color="#8257E5" />
-    </View>
-  )
-}
