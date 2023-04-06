9da0cddb50fb58b5afa9f42825dcaf38dd8f0614
Bruno Alves <bruno@sthorm.io>
2023-04-06 15:29:14 -0300

    fix: load fonts and preview ticket layout

--- apps/mobile/app/(drawer)/(tabs)/ticket.tsx
+++ apps/mobile/app/(drawer)/(tabs)/ticket.tsx
@@ -8,7 +8,7 @@ export default function Ticket() {
   const { data } = useQuery([QueryKeys.users], getUser)
 
   return (
-    <View className="bg-zinc-900 flex-1 items-center justify-center">
+    <View className="bg-gray-900 flex-1 items-center justify-center">
       <Text className="text-gray-100 font-bold text-2xl">Ingresso</Text>
 
       <Text className="text-gray-100 font-bold text-2xl">{data?.login}</Text>
@@ -16,11 +16,9 @@ export default function Ticket() {
       <Link className="text-blue-500" href="credential">
         Acessar credencial
       </Link>
-      <View className="bg-gray-900 flex-1 items-center justify-center">
-        <Text className="text-gray-100 font-bold text-2xl font-heading">
-          Ingresso
-        </Text>
-      </View>
+      <Text className="text-gray-100 font-bold text-2xl font-heading">
+        Ingresso
+      </Text>
     </View>
   )
 }
--- apps/mobile/app/(drawer)/_layout.tsx
+++ apps/mobile/app/(drawer)/_layout.tsx
@@ -1,12 +1,33 @@
+import { ActivityIndicator, View } from 'react-native'
 import { Drawer } from 'expo-router/drawer'
 import { StatusBar } from 'expo-status-bar'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import { purple } from 'tailwindcss/colors'
 import { QueryClientProvider } from '@tanstack/react-query'
-
 import { queryClient } from '../../src/lib/react-query'
 
 import CustomDrawerContent from './CustomDrawerContent'
 
 export default function DrawerLayout() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  })
+
+  if (!fontsLoaded) {
+    return (
+      <View className="flex-1 items-center justify-center bg-gray-950">
+        <ActivityIndicator color={purple[600]} size="large" />
+      </View>
+    )
+  }
+
   return (
     <QueryClientProvider client={queryClient}>
       <StatusBar style="light" backgroundColor="transparent" translucent />
