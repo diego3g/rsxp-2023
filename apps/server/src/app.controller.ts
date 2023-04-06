c8b6d88decb58ec415d9baa15462d65644eaafff
Vini Fraga <vinifragam@gmail.com>
2023-04-06 19:14:42 -0300

    feat: verify user clerk session

--- apps/server/src/app.controller.ts
+++ apps/server/src/app.controller.ts
@@ -1,16 +1,38 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Post, Req, Res } from '@nestjs/common'
+import { sessions, users } from '@clerk/clerk-sdk-node'
+import { Request, Response } from 'express'
+
 import { PrismaService } from './database/prisma.service'
 
 @Controller()
 export class AppController {
   constructor(private prisma: PrismaService) {}
 
-  @Get()
-  async getUsers() {
-    const users = await this.prisma.user.findMany()
+  @Post()
+  async getUser(@Req() request: Request, @Res() response: Response) {
+    const sessionId = request.body.sessionId
+    const sessionToken = request.body.sessionToken
 
-    return {
-      data: users,
+    if (typeof sessionId === 'string') {
+      const session = await sessions.verifySession(sessionId, sessionToken)
+
+      if (!session) {
+        return response.status(400).json({
+          error: 'Session not verified',
+        })
+      }
+
+      const user = await users.getUser(session.userId)
+
+      console.log(user)
+
+      return response.json({
+        message: 'Session verified',
+      })
     }
+
+    return response.status(400).json({
+      error: 'Session not verified',
+    })
   }
 }
