66f81abab0a413b33b6510bca95126e0d69377b4
Gabriel <gabrielvitor.frasao@gmail.com>
2023-04-10 16:18:02 -0300

    refactor(sympla): validate api return with zod schema

--- apps/server/src/sympla/sympla.service.ts
+++ apps/server/src/sympla/sympla.service.ts
@@ -1,12 +1,8 @@
 import { Injectable } from '@nestjs/common'
-import { symplaApi } from 'src/lib/axios'
-import { Participant } from './sympla.dto'
+import axios, { AxiosInstance } from 'axios'
+import { z } from 'zod'
 
-export interface ParticipantResponseSuccess {
-  data: Participant
-}
-
-interface ParticipantResponseError {
+export interface ParticipantResponseError {
   error: boolean
   code: number
   message: string
@@ -14,13 +10,36 @@ interface ParticipantResponseError {
 
 @Injectable()
 export class SymplaService {
+  private symplaAPIClient: AxiosInstance
+
+  constructor() {
+    this.symplaAPIClient = axios.create({
+      baseURL: 'https://api.sympla.com.br/public/v3',
+      headers: {
+        S_TOKEN: process.env.SYMPLA_TOKEN,
+      },
+    })
+  }
+
   public async getParticipantByTicketNumber(ticketNumber: number) {
     try {
-      const { data } = await symplaApi.get<ParticipantResponseSuccess>(
+      const { data } = await this.symplaAPIClient.get(
         `/events/${process.env.SYMPLA_EVENT_ID}/participants/ticketNumber/${ticketNumber}`,
       )
 
-      return data.data
+      const participantSchema = z.object({
+        id: z.number(),
+        order_id: z.string(),
+        order_status: z.string(),
+        order_date: z.string(),
+        first_name: z.string(),
+        last_name: z.string(),
+        email: z.string().email(),
+        ticket_number: z.string(),
+        ticket_num_qr_code: z.string(),
+      })
+
+      return participantSchema.parse(data.data)
     } catch (error) {
       return error.response.data as ParticipantResponseError
     }
