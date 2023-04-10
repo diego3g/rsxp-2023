91e0433e8cd634ea0e7304bf102da4022d41e7c8
Vinícius Fraga <vinifragam@gmail.com>
2023-04-10 16:41:21 -0300

    fix: remove import duplicate

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -3,7 +3,6 @@ import { ConfigModule } from '@nestjs/config'
 
 import { AppController } from './app.controller'
 import { PrismaService } from './database/prisma.service'
-import { ConfigModule } from '@nestjs/config'
 
 @Module({
   imports: [ConfigModule.forRoot()],
