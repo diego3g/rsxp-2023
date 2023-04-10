35a1d6a27cfba3ed19efdb6e03868361b41b9fdf
Gabriel <gabrielvitor.frasao@gmail.com>
2023-04-10 17:48:30 -0300

    resolve conflicts

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { PrismaService } from './database/prisma.service'
 import { SymplaService } from './sympla/sympla.service'
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [PrismaService, SymplaService],
 })
