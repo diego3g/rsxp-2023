a5d32bef31e387c48434ccb74bae75776b99a28a
Gabriel <gabrielvitor.frasao@gmail.com>
2023-04-10 13:59:51 -0300

    refactor: handle error with try/catch

--- apps/server/src/sympla/sympla.service.ts
+++ apps/server/src/sympla/sympla.service.ts
@@ -1,41 +1,28 @@
 import { Injectable } from '@nestjs/common'
 import { symplaApi } from 'src/lib/axios'
+import { Participant } from './sympla.dto'
 
-export interface Participant {
-  id: number
-  order_id: string
-  order_status: string
-  order_date: string
-  first_name: string
-  last_name: string
-  email: string
-  ticket_number: string
-  ticket_num_qr_code: string
-}
-
-export interface ParticipantSuccess {
+export interface ParticipantResponseSuccess {
   data: Participant
 }
 
-interface ParticipantError {
+interface ParticipantResponseError {
   error: boolean
   code: number
   message: string
 }
 
-type ParticipantResponse = ParticipantSuccess | ParticipantError
-
 @Injectable()
 export class SymplaService {
   public async getParticipantByTicketNumber(ticketNumber: number) {
-    const { data } = await symplaApi.get<ParticipantResponse>(
-      `/events/${process.env.SYMPLA_EVENT_ID}/participants/ticketNumber/${ticketNumber}`,
-    )
+    try {
+      const { data } = await symplaApi.get<ParticipantResponseSuccess>(
+        `/events/${process.env.SYMPLA_EVENT_ID}/participants/ticketNumber/${ticketNumber}`,
+      )
 
-    if ('error' in data) {
-      throw new Error(`[Sympla service] ${data.message}`)
+      return data.data
+    } catch (error) {
+      return error.response.data as ParticipantResponseError
     }
-
-    return { participant: data.data }
   }
 }
