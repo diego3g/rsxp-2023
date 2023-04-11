ceb0521eca39fa52ead1393bf42666a87b0dd76e
melquisedeque-magalhaes <melqui.sodre15@gmail.com>
2023-04-11 18:25:19 -0300

    chore(mobile): add scroll view in screen ticket

--- apps/mobile/src/app/(drawer)/(tabs)/ticket.tsx
+++ apps/mobile/src/app/(drawer)/(tabs)/ticket.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native'
+import { ScrollView, Text, View } from 'react-native'
 import { Ticket as TicketIcon, Question } from 'phosphor-react-native'
 
 import RocketImg from '@/assets/rocket.svg'
@@ -8,32 +8,37 @@ import { LinkButton } from '@/components/LinkButton'
 
 export default function Ticket() {
   return (
-    <View className="bg-gray-950 flex-1 px-5 py-10">
-      <RocketImg />
-      <Text className="text-gray-100 font-bold text-2xl mt-8">
-        Já possui um ingresso?
-      </Text>
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <View className="bg-gray-950 flex-1 px-5 py-10">
+        <RocketImg />
+        <Text className="text-gray-100 font-bold text-2xl mt-8">
+          Já possui um ingresso?
+        </Text>
 
-      <Text className="text-gray-200 mt-1 font-body text-base w-[312px] mb-5">
-        Vincule seu ingresso do Sympla com sua conta para facilitar seu check-in
-        no evento!
-      </Text>
+        <Text className="text-gray-200 mt-1 font-body text-base w-[312px] mb-5">
+          Vincule seu ingresso do Sympla com sua conta para facilitar seu
+          check-in no evento!
+        </Text>
 
-      <LinkButton.Root>
-        <LinkButton.Text>Como obter esse número?</LinkButton.Text>
-        <Question color={theme?.colors?.gray[300]} size={16} />
-      </LinkButton.Root>
+        <LinkButton.Root>
+          <LinkButton.Text>Como obter esse número?</LinkButton.Text>
+          <Question color={theme?.colors?.gray[300]} size={16} />
+        </LinkButton.Root>
 
-      <Button.Root className="mt-5">
-        <Button.Text>VINCULAR INGRESSO À CONTA</Button.Text>
-      </Button.Root>
+        <Button.Root className="mt-5">
+          <Button.Text>VINCULAR INGRESSO À CONTA</Button.Text>
+        </Button.Root>
 
-      <View className="my-4 h-[1px] bg-gray-700" />
+        <View className="my-4 h-[1px] bg-gray-700" />
 
-      <Button.Root variant="normal">
-        <TicketIcon color={theme?.colors?.white as string} />
-        <Button.Text>COMPRAR INGRESSO</Button.Text>
-      </Button.Root>
-    </View>
+        <Button.Root variant="normal">
+          <TicketIcon color={theme?.colors?.white as string} />
+          <Button.Text>COMPRAR INGRESSO</Button.Text>
+        </Button.Root>
+      </View>
+    </ScrollView>
   )
 }
