801b3edf5d2bf092109abc4c0c804238eee38c31
MathRMM <matheusrodrigomm@gmail.com>
2023-04-15 03:36:20 -0300

    feat(mobile): create Event Map screen

--- apps/mobile/src/app/(drawer)/eventMap.tsx
+++ apps/mobile/src/app/(drawer)/eventMap.tsx
@@ -1,16 +1,44 @@
-import { Button, Text, View } from 'react-native'
-import { useRouter } from 'expo-router'
+import { Text, View, Dimensions, ScrollView } from 'react-native'
+import ReactNativeZoomableView from '@openspacelabs/react-native-zoomable-view/src/ReactNativeZoomableView'
+
+import EventMapSVG from '@/assets/eventMap.svg'
+import LegendMap from '@/components/LegendMap'
 
 export default function EventMap() {
-  const { back } = useRouter()
+  const { width, height } = Dimensions.get('window')
+  const sizeHeight = (height / 100) * 60
 
   return (
     <View className="bg-gray-950 flex-1 items-start justify-start">
-      <Text className="text-gray-100 font-bold text-2xl my-8">
+      <Text className="text-gray-100 font-bold text-2xl my-8 left-4">
         Mapa do Evento
       </Text>
 
-      <Button title="Voltar" onPress={back} />
+      <View className="w-full" style={{ height: sizeHeight }}>
+        <ReactNativeZoomableView
+          maxZoom={3}
+          minZoom={0.5}
+          zoomStep={0.5}
+          initialZoom={1}
+          bindToBorders={true}
+          onZoomAfter={this.logOutZoomState}
+          style={{
+            padding: 2,
+          }}
+        >
+          <EventMapSVG
+            className="w-10 h-10"
+            width={width}
+            height={sizeHeight}
+          />
+        </ReactNativeZoomableView>
+      </View>
+      <ScrollView style={{ width: '100%', height: '100%' }}>
+        <View className="mx-4 mb-4 grid grid-cols-2" id="Legend Maps Field">
+          <LegendMap color={'#2EBCF1'}>Banheiro Masculino</LegendMap>
+          <LegendMap color={'#FF00FF'}>Banheiro Feminino</LegendMap>
+        </View>
+      </ScrollView>
     </View>
   )
 }
